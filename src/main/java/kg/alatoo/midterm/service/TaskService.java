e56e8b9a3789b860df32578199e5cbb21f2c6648
bakhtiiartashbolotov <amirarakieva@gmail.com>
2024-03-21 12:37:01 +0600

    Implemented services to handle business logic for tasks and users

--- src/main/java/kg/alatoo/midterm/service/TaskService.java
+++ src/main/java/kg/alatoo/midterm/service/TaskService.java
@@ -1,13 +1,33 @@
 package kg.alatoo.midterm.service;
 
+import kg.alatoo.midterm.entity.Task;
 import kg.alatoo.midterm.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
+
     private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskRepository.deleteById(id);
     }
 }
